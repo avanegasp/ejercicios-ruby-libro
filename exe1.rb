9d85c53ba6c408251e50f3b27640ee391001fbe3
avanegasp <arco527@gmail.com>
2018-01-06 12:12:26 -0500

    Second commit

--- exe1.rb
+++ exe1.rb
@@ -1,13 +1,47 @@
+r
 # Tu misión en este reto es crear un método que reciba un arreglo y retorne una cadena que contenga una frase con los elementos del arreglo.
 #
 # La frase se debe componer siguiendo las reglas del idioma español: los elementos se separan por coma (,), excepto los últimos dos que se separan con la letra y. Es decir ['hugo', 'paco', 'luis'] se convertirían en la frase "hugo, paco y luis".
 
 
-# puts ["a", "b", "c"].join("y")[4]
+# puts ["hugo", "paco", "luis"].join(",").last
 
 # puts ["a", "b", "c"].concat
 
 # puts ("hugo").insert(-2, "y")
+
+# puts " now's  the time".split(' ').join("y")
+
+def phrase (arr)
+    if arr == []
+      ""
+  elsif arr.length == 1
+    arr[0]
+    else
+   ultimo_elemento = arr.pop
+   arr.join(", ").concat(" y #{ultimo_elemento}")
+  end
+end
+puts phrase(['uno', 'dos', 'tres'])
+puts phrase(['uno'])
+puts phrase([]) # => ""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -16,14 +50,14 @@
 #
 # La función debe ajustar el texto a la longitud que se especifique en el segundo parámetro. Si la longitud de la cadena es mayor que el valor del segundo parámetro, se debe cortar. de lo contrario, se debe completar con espacios.
 
-
-def adjust_text (str, len)
-  if str.length > len
-    str.slice(len)
-  else
-    str.ljust(len)
-  end
-end
-
-
-puts adjust_text("hola",12)
+#
+# def adjust_text (str, len)
+#   if str.length > len
+#     str.slice(0, len)
+#   else
+#     str.ljust(len)
+#   end
+# end
+#
+#
+# puts adjust_text("hola",2)
